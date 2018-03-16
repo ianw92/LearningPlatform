3b3b8b20c835054e4c385b9d33d4e619db846144
Ian Williams <ianw92@hotmail.com>
2018-03-16 14:53:05 +0000

    Added static pages and moved calendar to it's own page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+
   resources :subtasks do
     post 'complete', on: :member
   end
   resources :tasks do
     post 'complete', on: :member
   end
+
   resources :todo_lists
-  # mount EpiCas::Engine, at: "/"
   resources :lecture_module_contents
   resources :lecture_modules
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/pages/*page" => "pages#show"
 
   root 'lecture_modules#index'
 end
