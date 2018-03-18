9e1e79eead74aedd5fa9a0bc7e553214fda41c53
Ian Williams <ianw92@hotmail.com>
2018-03-18 11:12:03 +0000

    Potential fix for postgresql issue on searching for todolists with user = *

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
       me = current_user
       my_username = User.find(me.id).username
       # TODO create methods in the models to get these lists
-      @todo_lists_global = TodoList.where("user = ?", my_username)
+      @todo_lists_global = TodoList.where("todo_lists.user = ?", my_username)
       logger.debug "Todo lists global: #{@todo_lists_global.ids}"
       @tasks_global = Task.where(todo_list_id: @todo_lists_global.pluck(:id)).order(:completed).order(:due_date)
       logger.debug "Tasks global: #{@tasks_global.ids}"
