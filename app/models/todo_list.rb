df0fc286b14fadfab498720ddac1d0e0c59cebd5
Ian Williams <ianw92@hotmail.com>
2018-03-19 08:50:43 +0000

    Minor changes. Correct routing to timetable page

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,12 +3,4 @@ class TodoList < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-
-  # validate :user_must_exist
-  #
-  # def user_must_exist
-  #   if User.where("username = ?", user).blank?
-  #     errors.add(:user, "doesn't exist")
-  #   end
-  # end
 end
