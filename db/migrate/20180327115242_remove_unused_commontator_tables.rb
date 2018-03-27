a1b6312e7824ee23909ee3012c65e38e2ad687f8
Ian Williams <ianw92@hotmail.com>
2018-03-27 17:03:07 +0100

    Removed bad migration

--- db/migrate/20180327115242_remove_unused_commontator_tables.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUnusedCommontatorTables < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :comments
-    drop_table :commontator_comments
-    drop_table :commontator_subscriptions
-    drop_table :commontator_threads
-  end
-end
